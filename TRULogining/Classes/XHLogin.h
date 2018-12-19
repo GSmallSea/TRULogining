d8f6077af6226abab06bfece159f14452e52f5ba
GSmallSea <gxiao_hai@163.com>
2018-12-19 15:31:26 +0800

    login

---
+++ TRULogining/Classes/XHLogin.h
@@ -0,0 +1,17 @@
+//
+//  XHLogin.h
+//  LZDemo
+//
+//  Created by XH on 2018/12/19.
+//  Copyright © 2018年 XH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XHLogin : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TRULogining/Classes/XHLogin.m
@@ -0,0 +1,13 @@
+//
+//  XHLogin.m
+//  LZDemo
+//
+//  Created by XH on 2018/12/19.
+//  Copyright © 2018年 XH. All rights reserved.
+//
+
+#import "XHLogin.h"
+
+@implementation XHLogin
+
+@end
